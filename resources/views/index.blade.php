02a3da5e6a5ea1ba3c9e7557f748ec4073f1340a
WenderOk <wondergeorgy@yandex.ru>
2025-02-25 23:50:59 +0000

    Footer_Done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <title>Studio2I</title>
-  <link rel="shortcut icon" type="image/svg" src="{{ asset('images/logo.svg') }}"/>
+  <link rel="shortcut icon" type="image/svg" src="{{ asset('images/logo.svg') }}" />
   @vite('resources/scss/app.scss')
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Geologica:wght@100..900&display=swap" />
   <link rel="stylesheet" href="{{ asset('css/Font-Awesome-6/css/all.css') }}" />
 </head>
+
 <body>
   <header class="header">
-    <div class="header__logo" ><img src="{{ asset('images/logo.svg') }}" alt="Агентство 2И" /></div>
+    <div class="header__logo"><img src="{{ asset('images/logo.svg') }}" alt="Агентство 2И" /></div>
     <nav class="header__menu">
       <div class="header__menu-element">
-        <a class="header__menu-link font--bold" href="/">Услуги</a>
+        <a class="text header__menu-link" href="/">Услуги</a>
       </div>
       <div class="header__menu-element">
-        <a class="header__menu-link font--bold" href="/">Портфолио</a>
+        <a class="text header__menu-link" href="/">Портфолио</a>
       </div>
       <div class="header__menu-element">
-        <a class="header__menu-link font--bold" href="#contacts">Контакты</a>
+        <a class="text header__menu-link" href="#contacts">Контакты</a>
       </div>
-      <a class="button button--transparent font--bold" href="tel:+79310093772>">
+      <a class="text button button--transparent" href="tel:+79310093772>">
         <div class="icon"><i class="fas fa-mobile-screen-button"></i></div>
         +7 931 009-37-72
       </a>
@@ -30,11 +33,11 @@
   </header>
 
   <div class="cover">
-    <p class="cover__heading">
+    <p class="text cover__heading">
       Разработка веб-сервисов<br />
       и сайтов с 2013 года
     </p>
-    <a href="tel:+79310093772" class="font--bold button button--blue">
+    <a href="tel:+79310093772" class="text button button--blue">
       <i class="fas fa-mobile-screen-button fa-fw"></i>
       +7 931 009-37-72
     </a>
@@ -42,52 +45,36 @@
 
   <footer class="footer">
     <div id="contacts" class="footer__contacts">
-      <div class="footer__contacts__group">
-        <div class="footer__contacts-heading">ОТДЕЛ ПРОДАЖ</div>
-        <div class="footer__contacts__group__contacts">
-          <div class="footer__contacts__group__contacts__phone">
-            <a class="footer__contacts__group__contacts__phone__text" href="tel:+79310093772">+7 931 009-37-72</a>
-          </div>
-          <div class="footer__contacts__group__contacts__social">
-            <div class="footer__contacts__group__contacts__social__link">
-              <i class="fa-solid fa-paper-plane"></i><a class="footer__contacts__group__contacts__social__link__text"
-                href="/">Telegram</a>
-            </div>
-            <div class="footer__contacts__group__contacts__social__link">
-              <i class="fa-brands fa-whatsapp"></i><a class="footer__contacts__group__contacts__social__link__text"
-                href="/">Whatsapp</a>
-            </div>
+      <div class="footer__contacts-group">
+        <p class="text footer__contacts-group-heading">ОТДЕЛ ПРОДАЖ</p>
+        <div class="footer__contacts-content">
+          <a class="text footer__contacts-phone" href="tel:+79310093772">+7 931 009-37-72</a>
+          <div class="footer__contacts-social">
+            <a class="text footer__contacts-social-link" href="/"><i class="fa-solid fa-paper-plane"></i>Telegram</a>
+            <a class="text footer__contacts-social-link" href="/"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
           </div>
         </div>
       </div>
-      <div class="footer__contacts__group">
-        <div class="footer__contacts-heading">СЕРВИСНЫЙ ОТДЕЛ</div>
-        <div class="footer__contacts__group__contacts">
-          <div class="footer__contacts__group__contacts__phone">
-            <a class="footer__contacts__group__contacts__phone__text" href="tel:+79310093772">+7 931 009-37-74</a>
-          </div>
-          <div class="footer__contacts__group__contacts__social">
-            <div class="footer__contacts__group__contacts__social__link">
-              <i class="fa-solid fa-paper-plane"></i><a class="footer__social-link-text" href="/">Telegram</a>
-            </div>
-            <div class="footer__contacts__group__contacts__social__link">
-              <i class="fa-brands fa-whatsapp"></i><a class="footer__social-link-text" href="/">Whatsapp</a>
-            </div>
+      <div class="footer__contacts-group">
+        <p class="text footer__contacts-group-heading">СЕРВИСНЫЙ ОТДЕЛ</p>
+        <div class="footer__contacts-content">
+          <a class="text footer__contacts-phone" href="tel:+79310093772">+7 931 009-37-7</a>
+          <div class="footer__contacts-social">
+            <a class="text footer__contacts-social-link" href="/"><i class="fa-solid fa-paper-plane"></i>Telegram</a>
+            <a class="text footer__contacts-social-link" href="/"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
           </div>
         </div>
       </div>
-      <div class="footer__contacts__mail-time">
-        <p class="footer__contacts__mail-time__mail">
-          <a class="footer__contacts__mail-time__mail__text" href="mailto:mail@studio2i.ru">mail@studio2i.ru</a>
-        </p>
-        <p class="footer__contacts__mail-time__time-text">
+      <div class="footer__contacts-mailAndTime">
+        <a class="text footer__contacts-mailAndTime-mail" href="mailto:mail@studio2i.ru">mail@studio2i.ru</a>
+        <p class="text footer__contacts-mailAndTime-time">
           По будням с 10:00 до 19:00 (по Москве)
         </p>
       </div>
     </div>
     <div class="footer__copyright">
-      <p class="footer__copyright__text">© 2013–2025 Диджитал-агентство 2И</p>
-      <p class="footer__copyright__text">
+      <p class="text footer__copyright-text">© 2013–2025 Диджитал-агентство 2И</p>
+      <p class="text footer__copyright-text">
         ИП Артамонов Иван Сергеевич<br />ОГРНИП 316470400116880
       </p>
     </div>
