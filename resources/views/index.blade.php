123f9ffa67e2e4d6db03ded57b5b0266af6f44bc
WenderOk <wondergeorgy@yandex.ru>
2025-03-25 18:54:36 +0000

    Footer_Adaptive_&_Other_Things

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,28 +11,28 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
-<body>
+<body class="body">
   <header class="header">
     <div class="header__logo"><img class="header__logo" src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Агентство 2И" /></div>
-    <nav class="menu">
-      <div class="menu__element">
-        <a class="menu__link" href="/">Услуги</a>
+    <nav class="header__menu">
+      <div class="header__menu-element">
+        <a class="header__menu-link" href="/">Услуги</a>
       </div>
-      <div class="menu__element">
-        <a class="menu__link" href="/">Портфолио</a>
+      <div class="header__menu-element">
+        <a class="header__menu-link" href="/">Портфолио</a>
       </div>
-      <div class="menu__element">
-        <a class="menu__link" href="#contacts">Контакты</a>
+      <div class="header__menu-element">
+        <a class="header__menu-link" href="#footer__contacts">Контакты</a>
       </div>
-      <button class="menu__burger-button">
-          <i class="fa-solid fa-bars button__icon"></i>
-          <p class="menu__burger-button-text">Меню</p>
+      <button class="header__burger-button">
+          <i class="fa-solid fa-bars header__burger-button-icon"></i>
+          <p class="header__burger-button-text">Меню</p>
       </button>
-      <a class="button button_transparent" href="tel:+79310093772>">
+      <a class="button button_transparent header__menu-button" href="tel:+79310093772>">
         <i class="fas fa-mobile-screen-button"></i>
         +7 931 009-37-72
       </a>
-      <a class="menu__phone-icon" href="tel:+79310093772"><i class="fas fa-mobile-screen-button"></i></a>
+      <a class="header__menu-phone-icon" href="tel:+79310093772"><i class="fas fa-mobile-screen-button"></i></a>
     </nav>
   </header>
 
@@ -41,37 +41,37 @@
       Разработка веб-сервисов<br />
       и сайтов с 2013 года
     </p>
-    <a href="tel:+79310093772" class="button button_blue">
+    <a href="tel:+79310093772" class="button button_blue cover__button">
       <i class="fas fa-mobile-screen-button fa-fw"></i>
       +7 931 009-37-72
     </a>
   </div>
 
   <footer class="footer">
-    <div id="contacts" class="contacts">
-      <div class="contacts__group">
-        <p class="contacts__group-heading">ОТДЕЛ ПРОДАЖ</p>
-        <div class="contacts__content">
-          <a class="contacts__phone" href="tel:+79310093772">+7 931 009-37-72</a>
-          <div class="contacts__social">
-            <a class="contacts__social-link" href="/"><i class="fa-solid fa-paper-plane"></i>Telegram</a>
-            <a class="contacts__social-link" href="/"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
+    <div id="footer__contacts" class="footer__contacts">
+      <div class="footer__contacts-group">
+        <p class="footer__contacts-heading">ОТДЕЛ ПРОДАЖ</p>
+        <div class="footer__contacts-content">
+          <a class="footer__contacts-phone" href="tel:+79310093772">+7 931 009-37-72</a>
+          <div class="footer__contacts-social">
+            <a class="footer__contacts-link" href="/"><i class="fa-solid fa-paper-plane"></i>Telegram</a>
+            <a class="footer__contacts-link" href="/"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
           </div>
         </div>
       </div>
-      <div class="contacts__group">
-        <p class="contacts__group-heading">СЕРВИСНЫЙ ОТДЕЛ</p>
-        <div class="contacts__content">
-          <a class="contacts__phone" href="tel:+79310093772">+7 931 009-37-74</a>
-          <div class="contacts__social">
-            <a class="contacts__social-link" href="/"><i class="fa-solid fa-paper-plane"></i>Telegram</a>
-            <a class="contacts__social-link" href="/"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
+      <div class="footer__contacts-group">
+        <p class="footer__contacts-heading">СЕРВИСНЫЙ ОТДЕЛ</p>
+        <div class="footer__contacts-content">
+          <a class="footer__contacts-phone" href="tel:+79310093772">+7 931 009-37-74</a>
+          <div class="footer__contacts-social">
+            <a class="footer__contacts-link" href="/"><i class="fa-solid fa-paper-plane"></i>Telegram</a>
+            <a class="footer__contacts-link" href="/"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
           </div>
         </div>
       </div>
-      <div class="contacts__mail-and-time">
-        <a class="contacts__mail" href="mailto:mail@studio2i.ru">mail@studio2i.ru</a>
-        <p class="contacts__time">
+      <div class="footer__contacts-mail-and-time">
+        <a class="footer__contacts-mail" href="mailto:mail@studio2i.ru">mail@studio2i.ru</a>
+        <p class="footer__contacts-time">
           По будням с 10:00 до 19:00 (по Москве)
         </p>
       </div>
